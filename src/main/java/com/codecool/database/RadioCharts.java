ea6b10a73485e9b84c4f509ed63736859922f2b5
Sirazad <lajosimola2@gmail.com>
2021-03-24 16:02:58 +0100

    pass empty DB tests

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,26 @@
 package com.codecool.database;
 
-
 public class RadioCharts {
-    //TODO: implement this class
+    private final String password;
+    private final String user;
+    private final String url;
+
+    public RadioCharts(String url, String user, String password) {
+        this.url=url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getMostPlayedSong(){
+        String mostPlayedSong = "";
+        // cím, név
+        return mostPlayedSong;
+    }
+
+    public String getMostActiveArtist(){
+        String mostPlayedArtist = "";
+        // előadó neve, akinek a legtöbb száma
+        return mostPlayedArtist;
+    }
+
 }
