710e14026aa0c93d7662073227baddf9228c9dc6
Sirazad <lajosimola2@gmail.com>
2021-03-24 16:56:38 +0100

    pass 3 tests

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,12 @@
 package com.codecool.database;
 
-public class RadioCharts {
+import com.sun.source.tree.UsesTree;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RadioCharts <T> {
     private final String password;
     private final String user;
     private final String url;
@@ -11,9 +17,30 @@ public class RadioCharts {
         this.password = password;
     }
 
-    public String getMostPlayedSong(){
+    public String getMostPlayedSong() {
         String mostPlayedSong = "";
-        // cím, név
+        List<Song> songs = new ArrayList();
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            String SQL = "SELECT song, SUM(times_aired) as times_aired FROM music_broadcast GROUP BY song";
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery(SQL);
+
+            int mostPlayed = 0;
+
+            while (results.next()) {
+                int currentPlayed = results.getInt("times_aired");
+                if ((currentPlayed > mostPlayed) && mostPlayedSong.equals("")) {
+                    mostPlayed = currentPlayed;
+                    mostPlayedSong = results.getString("song");
+                } else if ((currentPlayed > mostPlayed)) {
+
+                }
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return mostPlayedSong;
     }
 
